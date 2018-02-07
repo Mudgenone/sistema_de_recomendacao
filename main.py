69d6ced0051cf5394c72d1f8f1da4c356fab3271
Henrique Barbosa <mudgenone@gmail.com>
2018-02-07 01:17:42 -0200

    Recomendação para todos os usuários

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 import numpy as np
+from collections import defaultdict
 from surprise import Reader, Dataset, KNNBaseline
+
+def get_top_n(predictions, n=10):
+    '''Return the top-N recommendation for each user from a set of predictions.
+
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as
+            returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default
+            is 10.
+
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    '''
+
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
  
 reader = Reader(line_format='user item rating', sep=',', skip_lines=1, rating_scale=(0,5))
 data = Dataset.load_from_file('base.csv', reader=reader)
@@ -10,13 +37,15 @@ sim_options = {'name': 'pearson_baseline'}
 algo = KNNBaseline(sim_options=sim_options)
 algo.fit(trainset)
 
-#First User ID
-user_test_id = 413
+# Than predict ratings for all pairs (u, i) that are NOT in the training set.
+testset = trainset.build_anti_testset()
 
-user_test_neighbors = algo.get_neighbors(user_test_id, k=5)
+predictions = algo.test(testset)
 
-print('The 5 nearest neighbors of User Test are:')
-for user_id in user_test_neighbors:
-    print(user_id)
+top_n = get_top_n(predictions, n=5)
+
+# Print the recommended items for each user
+for uid, user_ratings in top_n.items():
+    print(uid, [iid for (iid, _) in user_ratings])
 
 
